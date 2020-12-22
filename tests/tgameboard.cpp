f9f42d7285676bad3142353aa9918b52e3f94c18
Matthew Esch <mze5111@gmail.com>
2020-12-22 15:15:58 -0500

    Merge part 4, readding tgameboard.cpp after casing issues deleted

---
+++ tests/tgameboard.cpp
@@ -0,0 +1,14 @@
+#include <gtest/gtest.h>
+#include <string>
+
+#include "gameboard.hpp"
+
+using std::string;
+
+TEST(Constructors, defaultMatchesFenStart) {
+    ASSERT_EQ(GameBoard(), GameBoard("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"));
+}
+
+TEST(FenNotationConstructor, badFen) {
+    ASSERT_DEATH(GameBoard("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"), "Assertion failed");
+}
