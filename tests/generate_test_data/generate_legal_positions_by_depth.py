4fa3913a4b35335d01924efad5907792feefe218
Andrew Duerig <gamechefproductions@gmail.com>
2020-12-25 07:50:01 -0500

    Add generate_legal_positions_by_depth

---
+++ tests/generate_test_data/generate_legal_positions_by_depth.py
@@ -0,0 +1,85 @@
+
+# uses https://python-chess.readthedocs.io/en/latest/_modules/chess.html 
+# to generate correct paths and legal moves
+import chess
+
+def fix_thing(board, popper, depth):
+    stop = False
+    while not stop:
+        first = False
+        popper.pop()
+        if not popper:
+            return False
+        board.pop()
+        last = popper[-1]
+        try:
+            the_move = next(last)
+            board.push(the_move)
+            popper.append(iter(list(board.legal_moves)))
+            stop = True
+        except:
+            if not popper:
+                return False
+    while len(popper) < depth:
+        the_move = next(popper[-1])
+        board.push(the_move)
+        popper.append(iter(list(board.legal_moves)))
+    return True
+
+def get_all_boards_at_depth(board, depth):
+    if depth <= 0:
+        yield 1
+        return
+    else:
+        popper = [iter(list(board.legal_moves))]
+
+        while len(popper) < depth:
+            the_move = next(popper[-1])
+            board.push(the_move)
+            popper.append(iter(list(board.legal_moves)))
+
+        while popper:
+            the_move = None
+            while the_move == None:
+                last = popper[-1]
+                try:
+                    the_move = next(last)
+                except:
+                    if not fix_thing(board, popper, depth):
+                        return
+            board.push(the_move)
+            yield 1
+            board.pop()
+
+board = chess.Board()
+total_move_counter = 0
+tracker = {}
+levels_to_search = 4
+
+get_num_dups = False
+if get_num_dups:
+    boards_set = set()
+    all_boards = []
+
+file_name = 'legal_positions_by_depth.dat'
+with open(file_name, 'w') as file:
+    for depth in range(1, levels_to_search):
+        depth_counter = 0
+        file.write('DEPTH: ' + str(depth) + '\n')
+        for _ in get_all_boards_at_depth(board, depth - 1):
+            all_legals = list(board.legal_moves)
+            for move in all_legals:
+                board.push(move)
+                if get_num_dups:
+                    all_boards.append(board.board_fen())
+                    boards_set.add(all_boards[-1])
+                file.write(board.board_fen() + '\n')
+                board.pop()
+                total_move_counter += 1
+                depth_counter += 1
+        tracker[depth] = depth_counter
+
+print(tracker)
+if get_num_dups:
+    print('total duplicates: {0:,}'.format(len(all_boards) - len(boards_set)))
+print('printed board fens out to', file_name)
--- tests/tgameboard.cpp
+++ tests/tgameboard.cpp
@@ -23,12 +23,7 @@ namespace ShumiChess {
                a.white_queens  == b.white_queens &&
                a.black_king    == b.black_king &&
                a.white_king    == b.white_king &&
-               a.turn          == b.turn &&
-               a.black_castle  == b.black_castle &&
-               a.white_castle  == b.white_castle &&
-               a.en_passant    == b.en_passant &&
-               a.halfmove      == b.halfmove &&
-               a.fullmove      == b.fullmove;
+               a.turn          == b.turn;
     }
 }
 
@@ -46,7 +41,7 @@ INSTANTIATE_TEST_CASE_P(BadFenConstructorParam, BadFenConstructor, testing::Valu
 
 typedef tuple<ull, ull, ull, ull, ull, ull, ull, ull, ull, ull, ull, ull, ShumiChess::Color> board_tuple;
 class tGameboardFenNotation : public testing::TestWithParam<pair<board_tuple, string>> {};
-TEST_P(tGameboardFenNotation, FenMatchesBitBoard) {
+TEST_P(tGameboardFenNotation, DefaultMatchesFenStart) {
     const auto test_pair = GetParam();
 
     ShumiChess::GameBoard fen_board {test_pair.second};
@@ -70,4 +65,8 @@ TEST_P(tGameboardFenNotation, FenMatchesBitBoard) {
 INSTANTIATE_TEST_CASE_P(tGameboardFenNotationParams,  
                         tGameboardFenNotation,  
                         testing::Values( make_pair(make_tuple(71776119061217280ULL, 65280ULL, 9295429630892703744ULL, 129ULL, 4755801206503243776ULL, 66ULL, 2594073385365405696ULL, 36ULL, 1152921504606846976ULL, 16ULL, 576460752303423488ULL, 8ULL, ShumiChess::Color::WHITE),
-                                                   "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1") ));
+                                                   "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1") ));  
+
+// TEST(Constructors, FenRepresentationCorrect) {
+//     ASSERT_EQ(ShumiChess::GameBoard(), ShumiChess::GameBoard("r3k2r/pp1n2pp/2p2q2/b2p1n2/BP1Pp3/P1N2P2/2PB2PP/R2Q1RK1 w kq b3 0 13"));
+// }
