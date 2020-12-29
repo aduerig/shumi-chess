119f9d83df883ec85c5f59570cb4ef9205157f85
Matthew Esch <mze5111@gmail.com>
2020-12-29 13:33:31 -0500

    Adds knight attack table

--- src/move_tables.cpp
+++ src/move_tables.cpp
@@ -19,4 +19,21 @@ const std::array<ull, 64> init_king_attack_table() {
     return king_attacks;
 }
 
+const std::array<ull, 64> init_knight_attack_table() {
+    std::array<ull, 64> knight_attacks = {};
+
+    for(int i=0; i < 64; ++i) {
+        if (i/8 < 7 && i%8 < 6) {knight_attacks[i] |= 1<<(i+10);} //2l 1u
+        if (i/8 < 6 && i%8 < 7) {knight_attacks[i] |= 1<<(i+17);} //1l 2u
+        if (i/8 < 6 && i%8 > 0) {knight_attacks[i] |= 1<<(i+15);} //1r 2u
+        if (i/8 < 7 && i%8 > 1) {knight_attacks[i] |= 1<<(i+6);} //2r 1u
+        if (i/8 > 0 && i%8 > 1) {knight_attacks[i] |= 1<<(i-10);} //2r 1d
+        if (i/8 > 1 && i%8 > 0) {knight_attacks[i] |= 1<<(i-17);} //1r 2d
+        if (i/8 > 1 && i%8 < 7) {knight_attacks[i] |= 1<<(i-15);} //1l 2d
+        if (i/8 > 0 && i%8 < 6) {knight_attacks[i] |= 1<<(i-6);} //2l 1d
+    }
+
+    return knight_attacks;
+}
+
 } // tables::movegen
--- src/move_tables.hpp
+++ src/move_tables.hpp
@@ -7,7 +7,8 @@
 namespace tables::movegen
 {
     const std::array<ull, 64> init_king_attack_table();
+    const std::array<ull, 64> init_knight_attack_table();
 
-    extern const std::array<ull, 64> king_attacks = init_king_attack_table();
-
+    extern const std::array<ull, 64> king_attack_table = init_king_attack_table();
+    extern const std::array<ull, 64> knight_attack_table = init_knight_attack_table();
 } // namespace tables::movegen
