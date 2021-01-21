3bc7ec46f707c12edb48197da124454e992bf447
Andrew Duerig <gamechefproductions@gmail.com>
2021-01-21 16:58:59 -0500

    Add gui infra improvements, adding new buttons is easier now, and only render piece changes that have moved

--- driver/show_board.py
+++ driver/show_board.py
@@ -21,9 +21,73 @@ for filepath in os.listdir(module_build_dir):
 
 import engine_communicator
 
-# print('this is from python')
-# engine_communicator.systemcall('echo "this is from the shell"')
-# engine_communicator.print_from_c()
+
+# ! button stuff
+class Button:
+    rectangle_graphics_object = None
+    text_graphics_object = None
+
+    def __init__(self, function_to_call, function_to_get_text, button_color, text_color):
+        self.function_to_call = function_to_call
+        self.function_to_get_text = function_to_get_text
+        self.button_color = button_color
+        self.text_color = text_color
+
+    def get_text(self):
+        return self.function_to_get_text()
+
+    def clicked(self):
+        return self.function_to_call(self)
+
+    def update_text(self):
+        self.text_graphics_object.setText(self.get_text())
+
+def clicked_pop_button(button_obj):
+    engine_communicator.pop()
+    undraw_pieces()
+    render_all_pieces_and_assign(board)
+    coord_focused = None
+    avail_moves = []
+
+def clicked_white_button(button_obj):
+    if type(both_players[0]) == Human:
+        both_players[0] = Random()
+    elif type(both_players[0]) == Random:
+        both_players[0] = Human()
+    button_obj.update_text()
+
+def clicked_black_button(button_obj):
+    if type(both_players[1]) == Human:
+        both_players[1] = Random()
+    elif type(both_players[1]) == Random:
+        both_players[1] = Human()
+    button_obj.update_text()
+
+def reset_board():
+    engine_communicator.reset_engine();
+    undraw_pieces()
+    render_all_pieces_and_assign(board)
+
+def clicked_reset_button(button_obj):
+    reset_board()
+
+global autoreset_toggle; autoreset_toggle = False
+def clicked_autoreset(button_obj):
+    global autoreset_toggle
+    if autoreset_toggle == False:
+        autoreset_toggle = True
+    else:
+        autoreset_toggle = False
+    button_obj.update_text()
+
+
+def gui_click_choices():
+    curr_y_cell = 8
+    for button in button_holder:
+        if raw_x > square_size * 8 and raw_x < 1 and raw_y < square_size * curr_y_cell and raw_y > square_size * (curr_y_cell - 1):
+            button.clicked()
+        curr_y_cell -= 1
+
 
 # ! some object types
 class PlayerType:
@@ -49,6 +113,7 @@ class AI(PlayerType):
 class Random(AI):
     def get_move(self):
         moves = engine_communicator.get_legal_moves()
+        # ! if this line errors it is because random.choice(moves) returns 0, which shouldn't really be possible in a completed engine
         choice = random.choice(moves)
         return choice[0:2], choice[2:4]
 
@@ -71,64 +136,35 @@ background = Rectangle(
 background.setFill(color_rgb(50, 50, 50))
 background.draw(win)
 
-# draws the pop button
-pop_button = Rectangle(
-    Point(square_size * 8, square_size * 8),
-    Point(1, square_size * 7)
-)
-pop_button.setFill(color_rgb(59, 48, 32))
-pop_button.draw(win)
 
-pop_text = Text(
-    Point(square_size * 9, square_size * 7.5), 
-    "pop!"
-)
-pop_text.setFill(color_rgb(200, 200, 200))
-pop_text.draw(win)
+# argument list: function to run on click, text, button_color, text color
+button_holder = [
+    Button(clicked_pop_button, lambda: "pop!", color_rgb(59, 48, 32), color_rgb(200, 200, 200)),
+    Button(clicked_white_button, lambda: "Black\n{}".format(both_players[0].get_name()), color_rgb(100, 100, 100), color_rgb(200, 200, 200)),
+    Button(clicked_black_button, lambda: "White\n{}".format(both_players[1].get_name()), color_rgb(20, 20, 20), color_rgb(200, 200, 200)),
+    Button(clicked_reset_button, lambda: "Reset", color_rgb(59, 48, 32), color_rgb(200, 200, 200)),
+    Button(clicked_autoreset, lambda: "autoreset: {}".format(autoreset_toggle), color_rgb(59, 48, 32), color_rgb(200, 200, 200))
+]
 
-# draws the color assosication buttons
-white_player_button = Rectangle(
-    Point(square_size * 8, square_size * 7),
-    Point(1, square_size * 6)
-)
-white_player_button.setFill(color_rgb(100, 100, 100))
-white_player_button.draw(win)
+curr_y_cell = 8
+for button_obj in button_holder:
+    new_button = Rectangle(
+        Point(square_size * 8, square_size * curr_y_cell),
+        Point(1, square_size * (curr_y_cell - 1))
+    )
+    new_button.setFill(button_obj.button_color)
+    new_button.draw(win)
 
-white_player_text = Text(
-    Point(square_size * 9, square_size * 6.5), 
-    "White\n{}".format(both_players[0].get_name())
-)
-white_player_text.setFill(color_rgb(200, 200, 200))
-white_player_text.draw(win)
+    new_button_text = Text(
+        Point(square_size * 9, square_size * (curr_y_cell - .5)), 
+        button_obj.get_text()
+    )
+    new_button_text.setFill(button_obj.text_color)
+    new_button_text.draw(win)
 
-black_player_button = Rectangle(
-    Point(square_size * 8, square_size * 6),
-    Point(1, square_size * 5)
-)
-black_player_button.setFill(color_rgb(20, 20, 20))
-black_player_button.draw(win)
-
-black_player_text = Text(
-    Point(square_size * 9, square_size * 5.5), 
-    "Black\n{}".format(both_players[1].get_name())
-)
-black_player_text.setFill(color_rgb(200, 200, 200))
-black_player_text.draw(win)
-
-# reset button
-reset_button = Rectangle(
-    Point(square_size * 8, square_size * 5),
-    Point(1, square_size * 4)
-)
-reset_button.setFill(color_rgb(59, 48, 32))
-reset_button.draw(win)
-
-reset_button_text = Text(
-    Point(square_size * 9, square_size * 4.5), 
-    "Reset"
-)
-reset_button_text.setFill(color_rgb(200, 200, 200))
-reset_button_text.draw(win)
+    button_obj.rectangle_graphics_object = new_button
+    button_obj.text_graphics_object = new_button_text
+    curr_y_cell -= 1
 
 # current turn text
 turn_text_values = {0: "White's turn", 1: "Black's turn"}
@@ -166,87 +202,89 @@ for file in os.listdir(os.path.join(actual_file_dir, 'images')):
         chess_image_filepaths[just_piece] = filepath
 
 # precompute chess coordinate notation to x, y values
-coord_to_x_y = {
+acn_to_x_y = {
     (str(chr(ord('a') + x)) + str(y+1)): (x, y) for y in range(8) for x in range(8)
 }
 
-x_y_to_coord = {
-    (x, y): (str(chr(ord('a') + x)) + str(y+1)) for y in range(8) for x in range(8)
-}
+
+x_y_to_acn = {}
+# reversing dict
+for acn, coord in acn_to_x_y.items():
+    x_y_to_acn[coord] = acn
+
 
 # pythons representation of the board
 board = {
     (str(chr(ord('a') + x)) + str(y+1)):None for y in range(8) for x in range(8)
 }
 
-# rendering pieces on boards
 
+# rendering pieces on boards
 def undraw_pieces():
-    for graphic in board.values():
-        if graphic:
-            graphic.undraw()
+    for value in board.values():
+        if value:
+            value[2].undraw()
     for i in range(len(board)):
         board[i] = None
 
-def render_pieces():
+
+def render_all_pieces_and_assign(board):
     positions = engine_communicator.get_piece_positions()
-    for piece_name, piece_coords in positions.items():
-        for coord in piece_coords:
-            x, y = coord_to_x_y[coord]
+    for piece_name, piece_acn in positions.items():
+        for acn in piece_acn:
+            x, y = acn_to_x_y[acn]
             location_of_image = Point(
                 (x / 10) + (square_size / 2), 
                 (y / 10) + (square_size / 2)
             )
             image_to_draw = Image(location_of_image, chess_image_filepaths[piece_name])
             image_to_draw.draw(win)
-            board[coord] = image_to_draw
-
-def update_pieces():
-    for graphic in board.values():
-        if graphic:
-            graphic.undraw()
-    for i in range(len(board)):
-        board[i] = None
-
-render_pieces()
+            board[acn] = (piece_name, acn, image_to_draw)
 
 
+def graphics_update_only_moved_pieces():
+    positions = engine_communicator.get_piece_positions()
+    still_taken_acns = set()
+    for piece_name, piece_acn in positions.items():
+        for acn in piece_acn:
+            if board[acn] == None:
+                x, y = acn_to_x_y[acn]
+                location_of_image = Point(
+                    (x / 10) + (square_size / 2), 
+                    (y / 10) + (square_size / 2)
+                )
+                image_to_draw = Image(location_of_image, chess_image_filepaths[piece_name])
+                image_to_draw.draw(win)
+                board[acn] = (piece_name, acn, image_to_draw)
+            elif board[acn][0] != piece_name:
+                board[acn][2].undraw()
+                x, y = acn_to_x_y[acn]
+                location_of_image = Point(
+                    (x / 10) + (square_size / 2), 
+                    (y / 10) + (square_size / 2)
+                )
+                image_to_draw = Image(location_of_image, chess_image_filepaths[piece_name])
+                image_to_draw.draw(win)
+                board[acn] = (piece_name, acn, image_to_draw)
+            still_taken_acns.add(acn)
+            
 
-def gui_click_choices():
-    # if click the pop button
-    if raw_x > square_size * 8 and raw_x < 1 and raw_y < square_size * 8 and raw_y > square_size * 7:
-        engine_communicator.pop()
-        undraw_pieces()
-        render_pieces()
-        coord_focused = None
-        avail_moves = []
-    # if click the white button
-    if raw_x > square_size * 8 and raw_x < 1 and raw_y < square_size * 7 and raw_y > square_size * 6:
-        if type(both_players[0]) == Human:
-            both_players[0] = Random()
-        elif type(both_players[0]) == Random:
-            both_players[0] = Human()
-        white_player_text.setText("White\n{}".format(both_players[0].get_name()))
-    # if click the black button
-    if raw_x > square_size * 8 and raw_x < 1 and raw_y < square_size * 6 and raw_y > square_size * 5:
-        if type(both_players[1]) == Human:
-            both_players[1] = Random()
-        elif type(both_players[1]) == Random:
-            both_players[1] = Human()
-        black_player_text.setText("Black\n{}".format(both_players[1].get_name()))
-    # if click the reset button
-    if raw_x > square_size * 8 and raw_x < 1 and raw_y < square_size * 5 and raw_y > square_size * 4:
-        engine_communicator.reset_engine();
-        undraw_pieces()
-        render_pieces()
+    for value in board.values():
+        if value:
+            piece_name, acn, graphics_object = value
+            if acn not in still_taken_acns:
+                graphics_object.undraw()
+                board[acn] = None
+
+
+render_all_pieces_and_assign(board)
+
 
 # ! playing loop for players
 player_index = 0
 coord_focused = None
 drawn_potential = []
 avail_moves = []
-
-
 while True:
     # 1 game iteration
     while engine_communicator.game_over() == -1:
@@ -260,8 +298,7 @@ while True:
         if isinstance(curr_player, AI):
             from_acn, to_acn = curr_player.get_move()
             engine_communicator.make_move_two_acn(from_acn, to_acn)
-            undraw_pieces()
-            render_pieces()
+            graphics_update_only_moved_pieces()
             player_index = 1 - player_index
             continue
 
@@ -276,16 +313,15 @@ while True:
         drawn_potential = []
 
         # get square_clicked_on in the gui
-        if (x, y) not in x_y_to_coord:
+        if (x, y) not in x_y_to_acn:
             gui_click_choices()
             continue
-        coord_clicked = x_y_to_coord[(x, y)]
+        coord_clicked = x_y_to_acn[(x, y)]
 
         if curr_player_type == Human:
             if coord_clicked in avail_moves: # if user inputs a valid move
                 engine_communicator.make_move_two_acn(coord_focused, coord_clicked)
-                undraw_pieces()
-                render_pieces()
+                graphics_update_only_moved_pieces()
                 coord_focused = None
                 avail_moves = []
                 player_index = 1 - player_index
@@ -306,7 +342,7 @@ while True:
             for move in legal_moves:
                 from_square, to_square = move[:2], move[2:]
                 if coord_focused == from_square:
-                    focused_x, focused_y = coord_to_x_y[to_square]
+                    focused_x, focused_y = acn_to_x_y[to_square]
                     avail_moves.append(to_square)
                     render_x = (focused_x * square_size) + square_size / 2
                     render_y = (focused_y * square_size) + square_size / 2
@@ -318,6 +354,11 @@ while True:
                     potential_move.draw(win)
                     drawn_potential.append(potential_move)
     
+    if autoreset_toggle:
+        print("automatically resetting board")
+        reset_board()
+        continue
+
     # get raw positions
     raw_position = win.getMouse()
     raw_x, raw_y = raw_position.x, raw_position.y
