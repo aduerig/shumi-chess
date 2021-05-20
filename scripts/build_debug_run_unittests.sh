99e62a5ec9ca9826307f2e173024a6542111fe37
Andrew Duerig <gamechefproductions@gmail.com>
2021-05-20 18:09:25 -0400

    Removing "RUNNING EXECUTABLE" from scipt

--- scripts/build_debug_run_unittests.sh
+++ scripts/build_debug_run_unittests.sh
@@ -2,4 +2,4 @@
 
 # -Wno-dev: supresses the deprecated warnings
 # -DBUILD_TESTS: controls if tests are built
-cmake CMakeLists.txt -Wno-dev -DBUILD_TESTS=ON && cmake --build . && ./bin/unit_tests --gtest_color=yes && echo "RUNNING EXECUTABLE" && ./bin/ok
+cmake CMakeLists.txt -Wno-dev -DBUILD_TESTS=ON && cmake --build . && ./bin/unit_tests --gtest_color=yes
