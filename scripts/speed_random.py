a702839a0b24fda9286967f9877160a719426b68
Andrew Duerig <duerig.andrew@gmail.com>
2023-11-01 21:07:33 -0400

    ok

--- scripts/speed_random.py
+++ scripts/speed_random.py
@@ -15,7 +15,6 @@ parser.add_argument('--debug', dest='release', default=True, action='store_false
 args = parser.parse_args()
 
 shared_build_code.build_shumi_chess(args.release, build_tests=False)
-shared_build_code.build_python_gui_module(args.release)
 run_command_blocking([
     str(shared_build_code.bin_dir.joinpath('measure_speed_random_games')),
 ], debug=True, stdout_pipe=None, stderr_pipe=None)
